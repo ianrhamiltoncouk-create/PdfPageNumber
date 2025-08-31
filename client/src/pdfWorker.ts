9d5ac1e89c03a2820c96cb97b3a4b38dbcb29ce1
ianrhamiltoncouk-create <ianrhamilton.co.uk@gmail.com>
2025-08-31 15:57:04 +0200

    Update pdfWorker.ts

--- client/src/pdfWorker.ts
+++ client/src/pdfWorker.ts
@@ -1,6 +1,6 @@
-// client/src/pdfWorker.ts
 import { GlobalWorkerOptions } from 'pdfjs-dist';
 import PdfJsWorker from 'pdfjs-dist/build/pdf.worker.min.mjs?worker';
 
-// Make the PDF.js worker match the library version bundled by Vite
+// Wire the worker Vite bundles to PDF.js
 GlobalWorkerOptions.workerPort = new PdfJsWorker();
+
