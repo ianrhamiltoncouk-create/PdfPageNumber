3e3c59bb961d553790e09a7277b5f3267d536868
ianrhamiltoncouk-create <ianrhamilton.co.uk@gmail.com>
2025-08-31 15:56:25 +0200

    Update main.tsx
    edit 11

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,7 @@
-import './pdfWorker'; // <-- add this FIRST
+import './pdfWorker'; // MUST be first
 
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
-
